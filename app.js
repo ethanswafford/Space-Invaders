fe0a57ca9a2e4b961281516d1a8ea540d7030999
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-24 23:04:20 -0500

    devPush

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const grid = document.querySelector('.grid');
 let currentShooterIndex = 202;
+let width = 15;
+let direction = 1;
+let invadersId;
+let goingRight = true;
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div')
@@ -22,4 +26,48 @@ function draw() {
 
 draw();
 
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove('invader');
+    }
+}
+
 squares[currentShooterIndex].classList.add('shooter');
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter');
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
+            break
+        case 'ArrowRight':
+            if (currentShooterIndex % width < width - 1) currentShooterIndex += 1;
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter');
+}
+document.addEventListener('keydown', moveShooter);
+
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0
+    const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1
+    remove();
+
+    if (rightEdge && goingRight) {
+        for (i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width + 1
+            direction = -1;
+            goingRight = false;
+        }
+    }
+
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction;
+    }
+
+    draw();
+
+}
+
+invadersId = setInterval(moveInvaders, 500);
